78c906c1775d8e1d6f4a4a10822a5f1aa76e7c98
WangChunye <wcy123@gmail.com>
2016-03-18 00:25:38 +0800

    rewrite it with gen_server

--- src/ekaf_server.erl
+++ src/ekaf_server.erl
@@ -1,24 +1,21 @@
 %%%-------------------------------------------------------------------
-%%% @author WangChunye <>
+%%% @author WangChunye <wcy123@gmail.com>
 %%% @copyright (C) 2016, WangChunye
 %%% @doc
 %%%
 %%% @end
-%%% Created : 17 Mar 2016 by WangChunye <>
+%%% Created : 18 Mar 2016 by WangChunye <>
 %%%-------------------------------------------------------------------
 -module(ekaf_server).
 
--behaviour(gen_fsm).
+-behaviour(gen_server).
 
 %% API
 -export([start_link/0]).
 
-%% gen_fsm callbacks
--export([init/1,
-         node_down/2, node_down/3,
-         node_up/2, node_up/3,
-         handle_event/3,
-         handle_sync_event/4, handle_info/3, terminate/3, code_change/4]).
+%% gen_server callbacks
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2,
+         terminate/2, code_change/3]).
 
 -define(SERVER, ?MODULE).
 
@@ -27,141 +24,112 @@
 %%%===================================================================
 %%% API
 %%%===================================================================
-send(Topic, Data) ->
-    gen_fsm:sync_send_event(?SERVER, {request, {kafka_send, Topic, Data}}).
 
 %%--------------------------------------------------------------------
 %% @doc
-%% Creates a gen_fsm process which calls Module:init/1 to
-%% initialize. To ensure a synchronized start-up procedure, this
-%% function does not return until Module:init/1 has returned.
+%% Starts the server
 %%
 %% @spec start_link() -> {ok, Pid} | ignore | {error, Error}
 %% @end
 %%--------------------------------------------------------------------
 start_link() ->
-    gen_fsm:start_link({local, ?SERVER}, ?MODULE, [], []).
+    gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
 
 %%%===================================================================
-%%% gen_fsm callbacks
+%%% gen_server callbacks
 %%%===================================================================
+
+send(Topic, Data) ->
+    gen_server:call(?SERVER, {request, {kafka_send, Topic, Data}}).
 
 %%--------------------------------------------------------------------
 %% @private
 %% @doc
-%% Whenever a gen_fsm is started using gen_fsm:start/[3,4] or
-%% gen_fsm:start_link/[3,4], this function is called by the new
-%% process to initialize.
+%% Initializes the server
 %%
-%% @spec init(Args) -> {ok, StateName, State} |
-%%                     {ok, StateName, State, Timeout} |
+%% @spec init(Args) -> {ok, State} |
+%%                     {ok, State, Timeout} |
 %%                     ignore |
-%%                     {stop, StopReason}
+%%                     {stop, Reason}
 %% @end
 %%--------------------------------------------------------------------
 init([]) ->
     debug("start to monirot ~p~n", [self()]),
     process_flag(trap_exit, true),
     ok = net_kernel:monitor_nodes(true, [{node_type, all}]),
-    {ok, node_down, #state{}, 0}.
+    {ok, #state{}}.
 
-node_down({request, Request}, From, State) ->
-    Port = maybe_start_node(State#state.port),
-    case State#state.pending_request of
-        undefined ->
-            {next_state, node_down, State#state{port = Port, pending_request = {From, Request}}};
-        _ ->
-            {reply, {error, node_down}, node_down, State}
-    end;
-node_down(Event, _From, State) ->
-    debug("UNKNOWN EVENT in state node_down: Event = ~p~n", [Event]),
-    {next_state, node_down, State}.
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling call messages
+%%
+%% @spec handle_call(Request, From, State) ->
+%%                                   {reply, Reply, State} |
+%%                                   {reply, Reply, State, Timeout} |
+%%                                   {noreply, State} |
+%%                                   {noreply, State, Timeout} |
+%%                                   {stop, Reason, Reply, State} |
+%%                                   {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
 
-node_down(Event, State) ->
-    debug("UNKNOWN ASYNC EVENT in state node_down: Event = ~p~n", [Event]),
-    {next_state, node_down, State}.
-
-node_up({request, Request}, From, State) ->
+handle_call({request, Request}, From, State) ->
     case is_node_alive() of
         true ->
             Reply = do_request(Request),
-            {reply, Reply, node_up, State};
+            {reply, Reply, State};
         false ->
-            Port = maybe_start_node(State#state.port),
-            {next_state, node_down, State#state{port = Port, pending_request = {From, Request}}}
+            case State#state.pending_request of
+                undefined ->
+                    Port = maybe_start_node(State#state.port),
+                    {noreply, State#state{port = Port, pending_request = {From, Request}}};
+                _OtherWise ->                   % only one pending request is allowed.
+                    {reply, {error, node_down}, State}
+            end
     end;
-node_up(Event, _From, State) ->
-    debug("UNKNOWN EVENT in state node_up: Event = ~p~n", [Event]),
-    {next_state, node_up, State}.
-
-node_up(Event, State) ->
-    debug("UNKNOWN ASYNC EVENT in state node_up: Event = ~p~n", [Event]),
-    {next_state, node_up, State}.
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Whenever a gen_fsm receives an event sent using
-%% gen_fsm:send_all_state_event/2, this function is called to handle
-%% the event.
-%%
-%% @spec handle_event(Event, StateName, State) ->
-%%                   {next_state, NextStateName, NextState} |
-%%                   {next_state, NextStateName, NextState, Timeout} |
-%%                   {stop, Reason, NewState}
-%% @end
-%%--------------------------------------------------------------------
-handle_event(_Event, StateName, State) ->
-    {next_state, StateName, State}.
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Whenever a gen_fsm receives an event sent using
-%% gen_fsm:sync_send_all_state_event/[2,3], this function is called
-%% to handle the event.
-%%
-%% @spec handle_sync_event(Event, From, StateName, State) ->
-%%                   {next_state, NextStateName, NextState} |
-%%                   {next_state, NextStateName, NextState, Timeout} |
-%%                   {reply, Reply, NextStateName, NextState} |
-%%                   {reply, Reply, NextStateName, NextState, Timeout} |
-%%                   {stop, Reason, NewState} |
-%%                   {stop, Reason, Reply, NewState}
-%% @end
-%%--------------------------------------------------------------------
-handle_sync_event(_Event, _From, StateName, State) ->
+handle_call(_Request, _From, State) ->
     Reply = ok,
-    {reply, Reply, StateName, State}.
+    {reply, Reply, State}.
 
 %%--------------------------------------------------------------------
 %% @private
 %% @doc
-%% This function is called by a gen_fsm when it receives any
-%% message other than a synchronous or asynchronous event
-%% (or a system message).
+%% Handling cast messages
 %%
-%% @spec handle_info(Info,StateName,State)->
-%%                   {next_state, NextStateName, NextState} |
-%%                   {next_state, NextStateName, NextState, Timeout} |
-%%                   {stop, Reason, NewState}
+%% @spec handle_cast(Msg, State) -> {noreply, State} |
+%%                                  {noreply, State, Timeout} |
+%%                                  {stop, Reason, State}
 %% @end
 %%--------------------------------------------------------------------
-handle_info({_Port, {data, Data}}, StateName, State) ->
-    debug("JAVANODE(~p): ~s~n", [StateName, Data]),
-    {next_state, StateName, State};
-handle_info({_Port, {exit_status,Status}}, StateName, State) ->
-    debug("JAVANODE(~p): exit = ~p~n", [StateName, Status]),
-    {next_state, StateName, State#state{port = undefined}};
-handle_info({nodedown, Node, _Info}, StateName, State) ->
+handle_cast(_Msg, State) ->
+    {noreply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling all non call/cast messages
+%%
+%% @spec handle_info(Info, State) -> {noreply, State} |
+%%                                   {noreply, State, Timeout} |
+%%                                   {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+handle_info({_Port, {data, Data}}, State) ->
+    debug("JAVANODE: ~s~n", [Data]),
+    {noreply, State};
+handle_info({_Port, {exit_status,Status}}, State) ->
+    debug("JAVANODE: exit = ~p~n", [Status]),
+    {noreply, State};
+handle_info({nodedown, Node, _Info}, State) ->
     debug("node down ~p~n", [Node]),
     case Node == get_java_node() of
         true ->
-            {next_state, node_down, State};
+            {stop, nodedown, State};
         false ->
-            {next_state, StateName, State}
+            {noreply, State}
     end;
-handle_info({nodeup, Node, _Info}, StateName, State) ->
+handle_info({nodeup, Node, _Info}, State) ->
     debug("node up ~p~n", [Node]),
     case Node == get_java_node() of
         true ->
@@ -172,28 +140,28 @@ handle_info({nodeup, Node, _Info}, StateName, State) ->
                 _ ->
                     ok
             end,
-            {next_state, node_up, State#state{pending_request = undefined}};
+            {noreply, State#state{pending_request = undefined}};
         false ->
-            {next_state, StateName, State}
+            {noreply, State}
     end;
-handle_info(Info, StateName, State) ->
-    debug("UNKNOWN INFO: Info = ~p, StateName = ~p~n", [Info, StateName]),
-    {next_state, StateName, State}.
+handle_info(Info, State) ->
+    debug("UNKNOWN INFO: Info = ~p~n", [Info]),
+    {noreply, State}.
+
 
 %%--------------------------------------------------------------------
 %% @private
 %% @doc
-%% This function is called by a gen_fsm when it is about to
+%% This function is called by a gen_server when it is about to
 %% terminate. It should be the opposite of Module:init/1 and do any
-%% necessary cleaning up. When it returns, the gen_fsm terminates with
-%% Reason. The return value is ignored.
+%% necessary cleaning up. When it returns, the gen_server terminates
+%% with Reason. The return value is ignored.
 %%
-%% @spec terminate(Reason, StateName, State) -> void()
+%% @spec terminate(Reason, State) -> void()
 %% @end
 %%--------------------------------------------------------------------
-terminate(Reason, StateName, State) ->
-    debug("SERVER EXIST: Reason = ~p, StateName = ~p~n",[Reason, StateName]),
-    catch erlang:port_close(State#state.port),
+terminate(Reason, _State) ->
+    debug("terminate with reason ~p~n", [Reason]),
     ok.
 
 %%--------------------------------------------------------------------
@@ -201,17 +169,15 @@ terminate(Reason, StateName, State) ->
 %% @doc
 %% Convert process state when code is changed
 %%
-%% @spec code_change(OldVsn, StateName, State, Extra) ->
-%%                   {ok, StateName, NewState}
+%% @spec code_change(OldVsn, State, Extra) -> {ok, NewState}
 %% @end
 %%--------------------------------------------------------------------
-code_change(_OldVsn, StateName, State, _Extra) ->
-    {ok, StateName, State}.
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
 
 %%%===================================================================
 %%% Internal functions
 %%%===================================================================
-
 maybe_start_node(Port) ->
     case erlang:port_info(Port) of
         undefined ->
@@ -274,7 +240,7 @@ try_produce_sync(Topic, Data) ->
             {error, timeout}
     end.
 
-%% debug(Fmt, Args) ->
-%%     io:format(Fmt, Args).
-debug(_Fmt, _Args) ->
-    ok.
+debug(Fmt, Args) ->
+     io:format(Fmt, Args).
+%% debug(_Fmt, _Args) ->
+%%     ok.
