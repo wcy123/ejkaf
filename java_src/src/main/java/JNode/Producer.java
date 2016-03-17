0c34d2a61ba0a0cff6dbefa1b3baaa0338fadab4
WangChunye <wcy123@gmail.com>
2016-03-18 00:52:09 +0800

    exit on kafka exception

--- java_src/src/main/java/JNode/Producer.java
+++ java_src/src/main/java/JNode/Producer.java
@@ -1,12 +1,14 @@
 package JNode;
 import kafka.producer.KeyedMessage;
 import kafka.producer.ProducerConfig;
+import org.apache.log4j.Logger;
 
 import java.util.Properties;
 
 class Producer
 {
-    private final kafka.javaapi.producer.Producer kafka;
+    private final kafka.javaapi.producer.Producer<String,String> kafka;
+    final static Logger logger = Logger.getLogger(Producer.class);
     public Producer()
     {
         Properties props = new Properties();
@@ -16,11 +18,17 @@ class Producer
             brokerList = "localhost:9092";
         }
         props.put("metadata.broker.list", brokerList);
-        this.kafka = new kafka.javaapi.producer.Producer(new ProducerConfig(props));
+        this.kafka = new kafka.javaapi.producer.Producer<String,String>(new ProducerConfig(props));
     }
 
     public void send(String topic, String messageStr) {
-        KeyedMessage msg = new KeyedMessage(topic, messageStr);
-        kafka.send(msg);
+        KeyedMessage<String,String> msg;
+        msg = new KeyedMessage<String,String>(topic, messageStr);
+        try {
+            kafka.send(msg);
+        }catch( Exception e) {
+            logger.error("cannot send msg " + msg, e);
+            System.exit(1);
+        }
     }
 }
